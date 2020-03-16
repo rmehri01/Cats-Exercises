2e84c90e8b59e5400e9cf40729bfab6887d69dce
Ryan Mehri <ryan.mehri1@gmail.com>
2020-03-16 09:44:39 -0700

    Add Id exercise

--- src/main/scala/sandbox/monad/Main.scala
+++ src/main/scala/sandbox/monad/Main.scala
@@ -1,5 +1,14 @@
 package sandbox.monad
 
 object Main extends App {
+  type Id[A] = A
 
+  def pure[A](a: A): Id[A] = a
+
+  def flatMap[A, B](value: Id[A])(func: A => Id[B]): Id[B] = func(value)
+
+  def map[A, B](value: Id[A])(func: A => B): Id[B] = func(value)
+
+  println(pure(2))
+  println(flatMap("foo")(_ + " bar"))
 }
