c359f61cdf5dfe801a2ae6effa68909d8bc6d2ae
Ryan Mehri <ryan.mehri1@gmail.com>
2020-03-22 10:44:34 -0700

    Add writer exercises

--- src/main/scala/sandbox/monad/Main.scala
+++ src/main/scala/sandbox/monad/Main.scala
@@ -1,7 +1,12 @@
 package sandbox.monad
 
 import cats.Eval
+import cats.data.Writer
 import cats.implicits._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Main extends App {
   // Id exercise
@@ -61,4 +66,49 @@ object Main extends App {
 
   //  naiveFoldRight((1 to 100000).toList, 0)(_ + _) – this stack overflows
   println(safeFoldRight((1 to 100000).toList, 0)(_ + _))
+
+  // Writer exercise
+  def slowly[A](body: => A) =
+    try body
+    finally Thread.sleep(100)
+
+  def factorial(n: Int): Int = {
+    val ans = slowly(if (n == 0) 1 else n * factorial(n - 1))
+    println(s"fact $n $ans")
+    ans
+  }
+
+  factorial(5)
+  Await.result(
+    Future.sequence(Vector(Future(factorial(3)), Future(factorial(3)))),
+    5.seconds
+  ) // logs are mixed up in multithreaded environments
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  def writerFactorial(n: Int): Logged[Int] = {
+    val ans = slowly(
+      if (n == 0) 1.pure[Logged] else writerFactorial(n - 1).map(_ * n)
+    )
+    ans.mapWritten(log => log :+ s"fact $n ${ans.value}")
+  }
+
+  Await.result(
+    Future.sequence(
+      Vector(
+        Future(println(writerFactorial(3).run)),
+        Future(println(writerFactorial(3).run))
+      )
+    ),
+    Duration.Inf
+  )
+
+  // their solution with for comprehension instead
+  def writerFactorial2(n: Int): Logged[Int] =
+    for {
+      ans <- if (n == 0) { 1.pure[Logged] } else {
+        slowly(writerFactorial2(n - 1).map(_ * n))
+      }
+      _ <- Vector(s"fact $n $ans").tell
+    } yield ans
 }
