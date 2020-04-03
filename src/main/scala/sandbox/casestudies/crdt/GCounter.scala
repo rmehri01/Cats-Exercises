24ebb0836c5b35d185680075ec58302b58261913
Ryan Mehri <ryan.mehri1@gmail.com>
2020-04-03 12:08:43 -0600

    Make GCounter into a type class

--- src/main/scala/sandbox/casestudies/crdt/GCounter.scala
+++ src/main/scala/sandbox/casestudies/crdt/GCounter.scala
@@ -3,16 +3,33 @@ package sandbox.casestudies.crdt
 import cats.kernel.CommutativeMonoid
 import cats.implicits._
 
-final case class GCounter[A](counters: Map[String, A]) {
-  def increment(machine: String,
-                amount: A)(implicit m: CommutativeMonoid[A]) = {
-    val value = amount |+| counters.getOrElse(machine, m.empty)
-    GCounter(counters + (machine -> value))
+trait GCounter[F[_, _], K, V] {
+  def increment(f: F[K, V])(k: K, v: V)(
+    implicit m: CommutativeMonoid[V]
+  ): F[K, V]
+  def merge(f1: F[K, V], f2: F[K, V])(
+    implicit b: BoundedSemiLattice[V]
+  ): F[K, V]
+  def total(f: F[K, V])(implicit m: CommutativeMonoid[V]): V
+}
+
+object GCounter {
+  def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]) =
+    counter
+
+  implicit def mapGCounter[A] = new GCounter[Map, String, A] {
+    override def increment(
+      f: Map[String, A]
+    )(k: String, v: A)(implicit m: CommutativeMonoid[A]): Map[String, A] = {
+      val value = v |+| f.getOrElse(k, m.empty)
+      f + (k -> value)
+    }
+
+    override def merge(f1: Map[String, A], f2: Map[String, A])(
+      implicit b: BoundedSemiLattice[A]
+    ): Map[String, A] = f1 |+| f2
+
+    override def total(f: Map[String, A])(implicit m: CommutativeMonoid[A]): A =
+      f.values.toList.combineAll
   }
-
-  def merge(that: GCounter[A])(implicit b: BoundedSemiLattice[A]): GCounter[A] =
-    GCounter(this.counters |+| that.counters)
-
-  def total(implicit m: CommutativeMonoid[A]): A =
-    counters.values.toList.combineAll
 }
--- src/main/scala/sandbox/casestudies/crdt/Main.scala
+++ src/main/scala/sandbox/casestudies/crdt/Main.scala
@@ -3,15 +3,12 @@ package sandbox.casestudies.crdt
 import cats.implicits._
 
 object Main extends App {
-  val gCounterInt1 = GCounter(Map("a" -> 2, "b" -> 5))
-  val gCounterInt2 = GCounter(Map("a" -> 5, "b" -> 3))
-  println(gCounterInt1.total)
-  println((gCounterInt1 merge gCounterInt2).counters)
-  println(gCounterInt1.increment("a", 5).counters)
+  val g1 = Map("a" -> 7, "b" -> 3)
+  val g2 = Map("a" -> 2, "b" -> 5)
+  val counter = GCounter[Map, String, Int]
+  val merged = counter.merge(g1, g2)
+  val total = counter.total(merged)
 
-  val gCounterSet1 = GCounter(Map("a" -> Set(2), "b" -> Set(1, 5)))
-  val gCounterSet2 = GCounter(Map("a" -> Set(5), "b" -> Set(3)))
-  println(gCounterSet1.total)
-  println((gCounterSet1 merge gCounterSet2).counters)
-  println(gCounterSet1.increment("a", Set(20)).counters)
+  println(merged)
+  println(total)
 }
