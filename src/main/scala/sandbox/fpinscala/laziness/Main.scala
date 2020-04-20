92da0ced9af3a390a1276ff29f6face39a76a0b5
Ryan Mehri <ryan.mehri1@gmail.com>
2020-04-19 18:28:31 -0600

    Start on laziness exercises

---
+++ src/main/scala/sandbox/fpinscala/laziness/Main.scala
@@ -0,0 +1,11 @@
+package sandbox.fpinscala.laziness
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val stream = Stream(1, 2, 3)
+    println(stream.toList)
+    println(stream.take(2).toList)
+    println(stream.drop(2).toList)
+  }
+
+}
