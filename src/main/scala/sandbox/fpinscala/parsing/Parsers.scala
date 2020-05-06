c80f89e2de08a575035c26bf17fd7cd4c7429d40
Ryan Mehri <ryan.mehri1@gmail.com>
2020-05-06 10:08:58 -0600

    Finish generalizing the parsing library

--- src/main/scala/sandbox/fpinscala/parsing/Parsers.scala
+++ src/main/scala/sandbox/fpinscala/parsing/Parsers.scala
@@ -3,6 +3,8 @@ package sandbox.fpinscala.parsing
 import sandbox.fpinscala.propertytesting._
 import Prop._
 
+import scala.util.matching.Regex
+
 trait Parsers[ParseError, Parser[+ _]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
@@ -20,7 +22,8 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
     loop(n, succeed(Nil))
   }
 
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
+    p.flatMap(a => succeed(f(a)))
 
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, p.many)(_ :: _) | succeed(Nil)
@@ -33,27 +36,34 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
   def many1[A](p: Parser[A]): Parser[List[A]] =
     map2(p, p.many)(_ :: _)
 
-  def product[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    for { a <- p; b <- p2 } yield (a, b)
 
-  def map2[A, B, C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
-    (p ** p2).map(f.tupled)
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+//    (p ** p2).map(f.tupled)
+    for { a <- p; b <- p2 } yield f(a, b)
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(
     implicit f: A => Parser[String]
   ): ParserOps[String] = ParserOps(f(a))
+  implicit def regex(r: Regex): Parser[String]
 
   case class ParserOps[A](p: Parser[A]) {
-    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
     def many: Parser[List[A]] = self.many(p)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
     def slice: Parser[String] = self.slice(p)
-    def **[B](pb: Parser[B]): Parser[(A, B)] = self.product(p, pb)
+    def **[B](pb: => Parser[B]): Parser[(A, B)] = self.product(p, pb)
     def many1: Parser[List[A]] = self.many1(p)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {
